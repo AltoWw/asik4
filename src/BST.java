86457f5b392c39a1d5c5e7f793dca6841807459f
Altynbek <rysbek20051@gmail.com>
2023-05-13 03:55:02 +0600

    put and get method

--- src/BST.java
+++ src/BST.java
@@ -1,23 +1,64 @@
-public class BST <K extends Comparable <K>, V> {
+import java.util.*;
+public class BST <K extends Comparable <K>, V> implements Iterable<K> {
     private Node root;
 
-    private class Node
-    {
+    private class Node {
         private K key;
         private V val;
+        private int size = 1;
         private Node left, right;
-        public Node(K key, V val)
-        {
+        public Node(K key, V val) {
             this.key = key;
             this.val = val;
         }
     }
     public void put(K key, V val) {
-
+        if(root == null) {
+            root = new Node(key, val);
+            return;
+        }
+        put(root, key, val);
     }
+
+    private void put(Node node, K key, V val) {
+        node.size++;
+        int comp = key.compareTo(node.key);
+        if (comp == 0) {
+            node.val = val;
+        } else if (comp > 0) {
+            if(node.right == null) {
+                node.right = new Node(key, val);
+            }
+            else {
+                put(node.right, key, val);
+            }
+        } else {
+            if(node.left == null) {
+                node.left = new Node(key, val);
+            }
+            else {
+                put(node.left, key, val);
+            }
+        }
+    }
+
     public V get(K key) {
-
+        return get(root, key);
     }
+
+    private V get(Node node, K key) {
+        if(node == null) return null;
+
+        int comp = key.compareTo(node.key);
+        if (comp == 0) {
+            return node.val;
+        } else if (comp > 0) {
+            return get(node.right, key);
+        } else {
+            return get(node.left, key);
+        }
+    }
+
     public void delete(K key) {
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        BST<Integer, Integer> a = new BST<>();
+        a.put(8,4235678);
+        a.put(3,5);
+        a.put(10,1023);
+        a.put(14,176);
+        a.put(13,163);
+        a.put(6,513);
+
+        for(Integer b : a) {
+            System.out.println(b);
+        }
 }
